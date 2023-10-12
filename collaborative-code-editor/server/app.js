e5a198721864fd1710f4cc81559f39ca573e91aa
Julio D. Chavez <chavezjulio67@gmail.com>
2023-10-11 23:18:35 -0500

    Register and Login

--- collaborative-code-editor/server/app.js
+++ collaborative-code-editor/server/app.js
@@ -1,6 +1,10 @@
 // server/app.js
 
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+
 const app = express();
 const port = 3001; // Change this to your desired port number
 
@@ -13,6 +17,12 @@ const authRoutes = require('./routes/auth');
 const sessionsRoutes = require('./routes/sessions');
 app.use('/auth', authRoutes);
 app.use('/sessions', sessionsRoutes);
+
+// Session and Passport initialization
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- collaborative-code-editor/server/routes/auth.js
+++ collaborative-code-editor/server/routes/auth.js
@@ -1,8 +1,26 @@
 // server/routes/auth.js
 
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 // Define authentication routes here
-
+router.get('/login', (req, res) => {
+    res.render('login'); // Render your login form
+  });
+  
+  router.post('/login', passport.authenticate('local', {
+    successRedirect: '/dashboard', // Redirect on successful login
+    failureRedirect: '/login',     // Redirect on failed login
+    failureFlash: true,
+  }));
+  
+  router.get('/register', (req, res) => {
+    res.render('register'); // Render your registration form
+  });
+  
+  router.post('/register', (req, res) => {
+    // Handle user registration, create a new User instance, and save it to the database
+  });
+  
 module.exports = router;
