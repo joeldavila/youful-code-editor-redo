bed4772f07ff166bb6b4b34490d967958cf0b547
Julio D. Chavez <chavezjulio67@gmail.com>
2023-10-12 21:55:14 -0500

    Creating Code-Editing Sessions

--- collaborative-code-editor/server/app.js
+++ collaborative-code-editor/server/app.js
@@ -85,7 +85,91 @@ app.get('/dashboard', ensureAuthenticated, (req, res) => {
     res.send('Welcome to the dashboard!');
 });
 
+const sessions = [];
 
+class CodeEditingSession {
+    constructor(sessionName, code, creator) {
+        this.id = sessions.length + 1;
+        this.sessionName = sessionName;
+        this.code = code;
+        this.creator = creator;
+        this.participants = [creator]; // Add the creator to the participants
+        this.permissions = { edit: [creator] }; // Initial permissions for the creator
+    }
+    // Add a user to the session with edit permissions
+    addUserWithEditPermission(user) {
+        this.participants.push(user);
+        this.permissions.edit.push(user);
+    }
+    // Remove edit permission from a user
+    removeEditPermission(user) {
+        const userIndex = this.permissions.edit.indexOf(user);
+        if (userIndex !== -1) {
+            this.permissions.edit.splice(userIndex, 1);
+        }
+    }
+    // Check if a user has edit permission
+    hasEditPermission(user) {
+        return this.permissions.edit.includes(user);
+    }
+}
+
+// Other imports and configurations for your Express app
+
+// Your routes and other application code
+
+// Start the server
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
+
+// Create a new session
+app.post('/sessions/create', ensureAuthenticated, (req, res) => {
+    const { sessionName, code } = req.body;
+    const newSession = new CodeEditingSession(sessionName, code, req.user);
+  
+    sessions.push(newSession);
+    res.redirect('/dashboard');
+});
+  
+// Join an existing session
+app.get('/sessions/:id/join', ensureAuthenticated, (req, res) => {
+    const sessionId = parseInt(req.params.id);
+    const session = sessions.find((s) => s.id === sessionId);
+  
+    if (session) {
+      session.participants.push(req.user);
+      res.redirect(`/sessions/${sessionId}`);
+    } else {
+      res.redirect('/dashboard');
+    }
+});
+  
+// Edit a session
+app.post('/sessions/:id/edit', ensureAuthenticated, (req, res) => {
+    const sessionId = parseInt(req.params.id);
+    const session = sessions.find((s) => s.id === sessionId);
+  
+    if (session && session.permissions.edit.includes(req.user)) {
+      session.code = req.body.code;
+      res.redirect(`/sessions/${sessionId}`);
+    } else {
+      res.redirect('/dashboard');
+    }
+});
+
+// Display session
+app.get('/sessions/:id', ensureAuthenticated, (req, res) => {
+    const sessionId = parseInt(req.params.id);
+    const session = sessions.find((s) => s.id === sessionId);
+  
+    if (session) {
+      res.render('session', { session });
+    } else {
+      res.redirect('/dashboard');
+    }
+});
+  
 
 app.use(passport.initialize());
 app.use(passport.session());
